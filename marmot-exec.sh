85d8130d4aaecdd0e9746eab730477a5d472300b
Kyle Krull <kdkrull@gmail.com>
2024-04-13 23:50:21 -0500

    Use subshells to preserve current directory after exec
    but it always lists the system version of node.  Is it not running
    direnv in the subshells?

--- marmot-exec.sh
+++ marmot-exec.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env zsh
 
 set -e
+
+
+printf "$HOME/ang/acb-lib: "
+(cd "$HOME/ang/acb-lib" && node --version)
+
+printf "$HOME/ang/angServer: "
+(cd "$HOME/ang/angServer" && node --version)
