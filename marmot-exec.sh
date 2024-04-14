ba22f543a54d1872122df1ce8b2eca5187f9b6da
Kyle Krull <kdkrull@gmail.com>
2024-04-14 01:07:15 -0500

    Read from hard-coded config file path

--- marmot-exec.sh
+++ marmot-exec.sh
@@ -8,14 +8,27 @@ then
   shift 1
 fi
 
-if (( $# < 1 ))
+if [[ "$1" == "--project-file" ]]
 then
-  echo "Usage: $0 [--direnv] <command> [args...]"
-  echo "Example: $0 --direnv node --version"
+  set -x
+  profile_file="$2"
+  shift 2
+
+  typeset -a project_repository_paths=("${(@f)"$(<conf/acb-project.conf)"}")
+  set +x
+else
+  echo "Missing: --project-file <file>"
   exit 1
 fi
 
-project_repository_paths=("$HOME/ang/acb-lib" "$HOME/ang/angServer")
+if (( $# < 1 ))
+then
+  echo "Usage: $0 [--direnv] --project-file <file> <command> [args...]"
+  echo "Example: $0 --project-file my-project.conf node --version"
+  exit 1
+fi
+
+# project_repository_paths=("$HOME/ang/acb-lib" "$HOME/ang/angServer")
 
 for repository_path in $project_repository_paths; do
   printf "$repository_path: "
