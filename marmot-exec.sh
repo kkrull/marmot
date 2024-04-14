d5d1494d6982536f9d24d7f283c200d86916b1a1
Kyle Krull <kdkrull@gmail.com>
2024-04-13 23:58:42 -0500

    Use current shell instead of sub-shell
    but this continues to show the default version of Node, not having run
    direnv.

--- marmot-exec.sh
+++ marmot-exec.sh
@@ -4,7 +4,10 @@ set -e
 
 
 printf "$HOME/ang/acb-lib: "
-(cd "$HOME/ang/acb-lib" && node --version)
+pushd . > /dev/null
+cd "$HOME/ang/acb-lib"
+node --version
+popd > /dev/null
 
 printf "$HOME/ang/angServer: "
 (cd "$HOME/ang/angServer" && node --version)
