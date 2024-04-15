b61429b8e20de060e29c49bd5095f96805429a79
Kyle Krull <kdkrull@gmail.com>
2024-04-14 19:12:49 -0500

    Remove stale code

--- src/exec/marmot-exec.sh
+++ src/exec/marmot-exec.sh
@@ -27,8 +27,6 @@ then
   exit 1
 fi
 
-# project_repository_paths=("$HOME/ang/acb-lib" "$HOME/ang/angServer")
-
 for repository_path in "${project_repository_paths[@]}"; do
   printf "%s: " "$repository_path"
   (cd "$repository_path" && "$@")
