629dcec80c091885b4357fd453af72ea5f925e40
Kyle Krull <kdkrull@gmail.com>
2024-05-03 17:03:21 -0500

    Avoid overwriting an existing meta repo

--- src/init/marmot-init.zsh
+++ src/init/marmot-init.zsh
@@ -3,7 +3,9 @@
 emulate -LR zsh
 
 self_invocation="marmot init"
+
 working_dirname="${PWD:A}"
+meta_repo_data="$working_dirname/.marmot"
 
 function main() {
   zparseopts -D -E \
@@ -13,16 +15,20 @@ function main() {
   then
     print_usage
     exit 0
+  elif [[ -d "$meta_repo_data" ]]
+  then
+    printf "Meta repo already exists: %s" "$meta_repo_data"
+    exit 1
   fi
 
-  create_meta_repo
+  create_meta_repo "$meta_repo_data"
 }
 
 function create_meta_repo() {
-  local meta_repo_data="$working_dirname/.marmot"
-  mkdir -p "$meta_repo_data"
+  local directory="$1"
+  mkdir -p "$directory"
 
-  local meta_repo_file="$meta_repo_data/meta-repo.json"
+  local meta_repo_file="$directory/meta-repo.json"
   echo "Hello World!" > "$meta_repo_file"
 
   echo "Initialized meta repository at $working_dirname"
