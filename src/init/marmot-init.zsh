30c4284ba3fdf40513fae1ec95215e60b896a0fb
Kyle Krull <kdkrull@gmail.com>
2024-05-03 16:47:47 -0500

    Add --help to marmot init

--- src/init/marmot-init.zsh
+++ src/init/marmot-init.zsh
@@ -2,10 +2,32 @@
 
 emulate -LR zsh
 
-#self_invocation="marmot init"
+self_invocation="marmot init"
 
 function main() {
+  zparseopts -D -E \
+    -help=help_option
+
+  if [[ -n "$help_option" ]]
+  then
+    print_usage
+    exit 0
+  fi
+
   echo "Hello world!"
+}
+
+
+function print_usage() {
+  cat >&2 <<-EOF
+${self_invocation} - Initialize a meta repository in the current directory
+
+SYNOPSIS
+${self_invocation} [--help]
+
+OPTIONS
+--help          Show help.
+EOF
 }
 
 main "$@"; exit
