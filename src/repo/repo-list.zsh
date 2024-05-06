26a42948df406b1ee2dee30dd6bc57306a156b07
Kyle Krull <kdkrull@gmail.com>
2024-05-06 14:28:33 -0500

    List managed repositories

--- src/repo/repo-list.zsh
+++ src/repo/repo-list.zsh
@@ -4,9 +4,9 @@ emulate -LR zsh
 
 self_invocation="marmot repo list"
 
-#working_dirname="${PWD:A}"
-#meta_repo_data="$working_dirname/.marmot"
-#meta_repo_config="$meta_repo_data/meta-repo.json"
+working_dirname="${PWD:A}"
+meta_repo_data="$working_dirname/.marmot"
+meta_repo_config="$meta_repo_data/meta-repo.json"
 
 function main() {
   if [[ $# == 0 ]]
@@ -28,12 +28,10 @@ function main() {
 }
 
 function list_local_repositories() {
-  local repository_home
-  repository_home="$1"
-
-  # TODO KDK: Query local meta data instead of doing find/exec/sh
-  find -s "$repository_home" -name '.git' -print -type d \
-    | sed 's/[/][.]git$//g'
+  # Treat lack of JSON fields as empty rather than as an error
+  # https://github.com/jqlang/jq/issues/354#issuecomment-43147898
+  jq < "$meta_repo_config" \
+    -r '.meta_repo.repositories[]?.name'
 }
 
 function print_usage() {
@@ -43,14 +41,12 @@ ${self_invocation} - List repositories
 SYNOPSIS
 ${self_invocation}
 ${self_invocation} [--help]
-${self_invocation} [--host=github] <Host URL>
 
 DESCRIPTION
 This command lists repositories that exist on the specified host.
 
 OPTIONS
 --help        Show help
---host        The type of host.  One of: github
 EOF
 }
 
