4a35bb8915b01a56597957275cf5f57166f06921
Kyle Krull <kdkrull@gmail.com>
2024-05-07 14:29:21 -0500

    Add example

--- src/cmd/repo/repo-register.zsh
+++ src/cmd/repo/repo-register.zsh
@@ -43,6 +43,12 @@ This command registers 1 or more repositories with Marmot, so it can manage them
 
 OPTIONS
 --help        Show help
+
+EXAMPLES
+• Register all the things!
+    \$ find -s ~/git -type d -name .git \\
+      | sed 's/[/][.]git$//g' \\
+      | xargs -I {} $_MARMOT_INVOCATION {}
 EOF
 }
 
