83c0a4fa58f4afbae49d2b81a724084f318a46f2
Kyle Krull <kdkrull@gmail.com>
2024-05-08 14:24:36 -0500

    Introduce --category option

--- src/cmd/repo/repo-list.zsh
+++ src/cmd/repo/repo-list.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 emulate -LR zsh
+set -e
 
 source "$_MARMOT_HOME/lib/config.zsh"
 source "$_MARMOT_HOME/lib/fs.zsh"
@@ -13,21 +14,27 @@ export _MARMOT_INVOCATION="${_MARMOT_INVOCATION} list"
 ## Command
 
 function main() {
-  if [[ $# == 0 ]]
-  then
-    list_local_repositories "$(_fs_metadata_file)"
-    exit 0
-  fi
-
   zparseopts -D -E \
+    -category:=category_option \
     -help=help_option
+
   if [[ -n "$help_option" ]]
   then
     print_usage
     exit 0
-  else
+  elif [[ $# -gt 0 ]]
+  then
     echo "Unknown option: $1"
     exit 1
+  elif [[ -n "$category_option" ]]
+  then
+    local category_or_subcategory
+    category_or_subcategory="${category_option[2]}"
+    echo "[list_local_repositories] category=$category_or_subcategory"
+    exit 0
+  else
+    list_local_repositories "$(_fs_metadata_file)"
+    exit 0
   fi
 }
 
@@ -45,13 +52,20 @@ $_MARMOT_INVOCATION - List repositories
 
 SYNOPSIS
 $_MARMOT_INVOCATION --help
-$_MARMOT_INVOCATION
+$_MARMOT_INVOCATION [--category <category|sub-category>]
 
 DESCRIPTION
-This command lists repositories that are managed by Marmot.
+This command lists repositories that have been registered with Marmot.
+Given options, this lists only the repositories that match the given criteria.
 
 OPTIONS
+--category    List repositories that have been added to the given category
+              or sub-category.
 --help        Show help
+
+EXAMPLES
+• List registered TypeScript repositories
+  \$ $_MARMOT_INVOCATION --category lang/typescript
 EOF
 }
 
