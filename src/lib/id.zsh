2d28159860e1eea7400d68714fb9c5ef0c9f8870
Kyle Krull <kdkrull@gmail.com>
2024-05-22 18:06:58 -0500

    Fix bug where the last variable was getting all the words

--- src/lib/id.zsh
+++ src/lib/id.zsh
@@ -4,7 +4,7 @@
 function _id_category_name() {
   local category_or_subcategory
   category_or_subcategory="$1"
-  IFS='/' read -r category_name <<< "$category_or_subcategory"
+  IFS='/' read -r category_name subcategory_name <<< "$category_or_subcategory"
   echo "$category_name"
 }
 
