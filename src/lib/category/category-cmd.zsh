05264c86fe0c6d10f6c8abd4e2fe2998674d452f
Kyle Krull <kdkrull@gmail.com>
2024-05-30 00:32:49 -0500

    Start moving commands over to another file

--- src/lib/category/category-cmd.zsh
+++ src/lib/category/category-cmd.zsh
@@ -1,2 +1,23 @@
 
 # Category commands
+
+function _categorycmd_rm() {
+  local category_or_subcategory="$1" ; shift 1
+
+  _categorymd_remove_repositories_as_local_paths \
+    "$(_fs_metadata_file)" \
+    "$category_or_subcategory" \
+    "${@:#}"
+  rm_link_to_category "$category_or_subcategory" "${@:#}"
+}
+
+function rm_link_to_category() {
+  local category_name="$1" ; shift 1
+
+  local link_path repository_path
+  for repository_path in "${@:#}"
+  do
+    link_path="$(_categoryfs_rm_repository_link "$category_name" "$repository_path")"
+    echo "- ${link_path} (link)"
+  done
+}
