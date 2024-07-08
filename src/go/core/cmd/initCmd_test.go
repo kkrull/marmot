2f77a64d28f345712229dc9a10a7ad9eadf29eb5
Kyle Krull <kdkrull@gmail.com>
2024-07-08 12:45:19 -0500

    Stub in test for intended behavior

--- src/go/core/cmd/initCmd_test.go
+++ src/go/core/cmd/initCmd_test.go
@@ -13,5 +13,9 @@ var _ = Describe("cmd.InitCmd", func() {
 			subject := cmd.InitCmd{}
 			Expect(subject.Run()).To(Succeed())
 		})
+
+		// Test the logic only, using an interface to check for the existence of the directory and data files
+		PIt("creates a directory, when none exists")
+		PIt("initializes meta data, when none exists")
 	})
 })
