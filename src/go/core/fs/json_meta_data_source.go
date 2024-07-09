b5379c7ade1c66e088dd5448992c7284e13bbac4
Kyle Krull <kdkrull@gmail.com>
2024-07-09 11:06:38 -0500

    Rename fields

--- src/go/core/fs/json_meta_data_source.go
+++ src/go/core/fs/json_meta_data_source.go
@@ -10,27 +10,25 @@ import (
 
 // Stores meta data in JSON files in a directory that Marmot manages
 type JsonMetaDataSource struct {
-	Path string
+	RepositoryPath string
 }
 
 func (source *JsonMetaDataSource) Init() error {
-	_, statErr := os.Stat(source.Path)
+	_, statErr := os.Stat(source.RepositoryPath)
 	if errors.Is(statErr, fs.ErrNotExist) {
-		return createMetaData(filepath.Join(source.Path, ".marmot"))
+		return createMetaData(filepath.Join(source.RepositoryPath, ".marmot"))
 	} else if statErr != nil {
 		return statErr
 	} else {
-		return fmt.Errorf("%s: path already exists", source.Path)
+		return fmt.Errorf("%s: path already exists", source.RepositoryPath)
 	}
 }
 
 func createMetaData(metaDataDir string) error {
+	metaDataFile := filepath.Join(metaDataDir, "meta-repo.json")
 	if dirErr := os.MkdirAll(metaDataDir, fs.ModePerm); dirErr != nil {
 		return fmt.Errorf("createMetaData %s: %w", metaDataDir, dirErr)
-	}
-
-	metaDataFile := filepath.Join(metaDataDir, "meta-repo.json")
-	if _, fileErr := os.Create(metaDataFile); fileErr != nil {
+	} else if _, fileErr := os.Create(metaDataFile); fileErr != nil {
 		return fmt.Errorf("createMetaData %s: %w", metaDataFile, fileErr)
 	}
 
