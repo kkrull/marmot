e379df0ab790176bc5d3fea0c82ed91226c3f344
Kyle Krull <kdkrull@gmail.com>
2024-07-12 23:16:34 -0500

    Move features/

--- src/go/main_test.go
+++ src/go/main_test.go
@@ -14,7 +14,7 @@ func TestFeatures(t *testing.T) {
 	suite := godog.TestSuite{
 		Options: &godog.Options{
 			Format:   "pretty",
-			Paths:    []string{"features"},
+			Paths:    []string{"feature"},
 			TestingT: t,
 		},
 		ScenarioInitializer: InitializeScenario,
