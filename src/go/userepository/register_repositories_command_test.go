a2e901cd816ff182a0274b9373c866d1e4bfa66e
Kyle Krull <kdkrull@gmail.com>
2024-07-14 11:05:00 -0500

    Pick next place to work

--- src/go/userepository/register_repositories_command_test.go
+++ src/go/userepository/register_repositories_command_test.go
@@ -2,11 +2,13 @@ package userepository_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("RegisterRepositoriesCommand", func() {
 	Describe("#Run", func() {
-		It("registers the given paths as local repositories, given paths to Git repositories", Pending, func() {
+		It("registers the given paths as local repositories, given paths to Git repositories", func() {
+			Expect("pending").To(Equal("passing"))
 		})
 
 		It("registers no remote URLs for a repository, given a Git repository with no remotes", Pending, func() {
