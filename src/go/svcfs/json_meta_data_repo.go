53e7ab7689d1b444f49a4d388747bc471860aae6
Kyle Krull <kdkrull@gmail.com>
2024-07-17 08:46:59 -0500

    Add AppendRemoteRepository

--- src/go/svcfs/json_meta_data_repo.go
+++ src/go/svcfs/json_meta_data_repo.go
@@ -8,10 +8,10 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/kkrull/marmot/corerepository"
+	core "github.com/kkrull/marmot/corerepository"
 )
 
-// A meta repo that stores meta data in JSON files on the file system.
+// A meta repo that stores meta data in JSON files in the specified directory.
 func NewJsonMetaDataRepo(repositoryPath string) *JsonMetaDataRepo {
 	metaDataDir := filepath.Join(repositoryPath, ".marmot")
 	return &JsonMetaDataRepo{
@@ -29,23 +29,23 @@ type JsonMetaDataRepo struct {
 
 /* MetaDataAdmin */
 
-func (meta *JsonMetaDataRepo) Init() error {
-	_, statErr := os.Stat(meta.repositoryDir)
+func (repo *JsonMetaDataRepo) Init() error {
+	_, statErr := os.Stat(repo.repositoryDir)
 	if errors.Is(statErr, fs.ErrNotExist) {
-		return meta.initDirectory()
+		return repo.initDirectory()
 	} else if statErr != nil {
-		return fmt.Errorf("failed to check for existing meta repo %s; %w", meta.repositoryDir, statErr)
+		return fmt.Errorf("failed to check for existing meta repo %s; %w", repo.repositoryDir, statErr)
 	} else {
-		return fmt.Errorf("path already exists: %s", meta.repositoryDir)
+		return fmt.Errorf("path already exists: %s", repo.repositoryDir)
 	}
 }
 
-func (meta *JsonMetaDataRepo) initDirectory() error {
+func (repo *JsonMetaDataRepo) initDirectory() error {
 	emptyFile := EmptyMetaRepoFile("0.0.1")
-	if dirErr := os.MkdirAll(meta.metaDataDir, fs.ModePerm); dirErr != nil {
-		return fmt.Errorf("failed to make directory %s; %w", meta.metaDataDir, dirErr)
-	} else if writeErr := emptyFile.WriteTo(meta.metaDataFile); writeErr != nil {
-		return fmt.Errorf("failed to write file %s; %w", meta.metaDataFile, writeErr)
+	if dirErr := os.MkdirAll(repo.metaDataDir, fs.ModePerm); dirErr != nil {
+		return fmt.Errorf("failed to make directory %s; %w", repo.metaDataDir, dirErr)
+	} else if writeErr := emptyFile.WriteTo(repo.metaDataFile); writeErr != nil {
+		return fmt.Errorf("failed to write file %s; %w", repo.metaDataFile, writeErr)
 	} else {
 		return nil
 	}
@@ -53,9 +53,9 @@ func (meta *JsonMetaDataRepo) initDirectory() error {
 
 /* RepositorySource */
 
-func (metaRepo *JsonMetaDataRepo) List() (corerepository.Repositories, error) {
-	if rootObject, readErr := ReadMetaRepoFile(metaRepo.metaDataFile); readErr != nil {
-		return nil, fmt.Errorf("failed to read file %s; %w", metaRepo.metaDataFile, readErr)
+func (repo *JsonMetaDataRepo) List() (core.Repositories, error) {
+	if rootObject, readErr := ReadMetaRepoFile(repo.metaDataFile); readErr != nil {
+		return nil, fmt.Errorf("failed to read file %s; %w", repo.metaDataFile, readErr)
 	} else if repositories, mapErr := rootObject.MetaRepo.MapRemoteRepositories(); mapErr != nil {
 		return nil, fmt.Errorf("failed to map to core model; %w", mapErr)
 	} else {
@@ -63,20 +63,16 @@ func (metaRepo *JsonMetaDataRepo) List() (corerepository.Repositories, error) {
 	}
 }
 
-func (metaRepo *JsonMetaDataRepo) RegisterRemote(hostUrl *url.URL) error {
+func (repo *JsonMetaDataRepo) RegisterRemote(hostUrl *url.URL) error {
 	var rootObject *rootObjectData
-	rootObject, readErr := ReadMetaRepoFile(metaRepo.metaDataFile)
+	rootObject, readErr := ReadMetaRepoFile(repo.metaDataFile)
 	if readErr != nil {
-		return fmt.Errorf("failed to read file %s; %w", metaRepo.metaDataFile, readErr)
+		return fmt.Errorf("failed to read file %s; %w", repo.metaDataFile, readErr)
 	}
 
-	rootObject.MetaRepo.RemoteRepositories = append(
-		rootObject.MetaRepo.RemoteRepositories,
-		remoteRepositoryData{Url: hostUrl.String()},
-	)
-
-	if writeErr := rootObject.WriteTo(metaRepo.metaDataFile); writeErr != nil {
-		return fmt.Errorf("failed to write file %s; %w", metaRepo.metaDataFile, writeErr)
+	rootObject.MetaRepo.AppendRemoteRepository(remoteRepositoryData{Url: hostUrl.String()})
+	if writeErr := rootObject.WriteTo(repo.metaDataFile); writeErr != nil {
+		return fmt.Errorf("failed to write file %s; %w", repo.metaDataFile, writeErr)
 	} else {
 		return nil
 	}
--- src/go/svcfs/meta_repo_file.go
+++ src/go/svcfs/meta_repo_file.go
@@ -1,19 +1,23 @@
 package svcfs
 
+/* Constructors */
+
 // Construct a meta repo file with only minimal information; e.g. no Git repositories.
 func EmptyMetaRepoFile(version string) *rootObjectData {
 	return &rootObjectData{
-		MetaRepo: metaRepoData{
+		MetaRepo: &metaRepoData{
 			RemoteRepositories: make([]remoteRepositoryData, 0),
 		},
 		Version: version,
 	}
 }
 
+/* Structure */
+
 // Top-level object in the meta data file and its representation in JSON.
 type rootObjectData struct {
-	MetaRepo metaRepoData `json:"meta_repo"`
-	Version  string       `json:"version"`
+	MetaRepo *metaRepoData `json:"meta_repo"`
+	Version  string        `json:"version"`
 }
 
 type metaRepoData struct {
@@ -22,4 +26,10 @@ type metaRepoData struct {
 
 type remoteRepositoryData struct {
 	Url string `json:"url"`
+}
+
+/* Updates */
+
+func (metaRepo *metaRepoData) AppendRemoteRepository(remoteRepository remoteRepositoryData) {
+	metaRepo.RemoteRepositories = append(metaRepo.RemoteRepositories, remoteRepository)
 }
