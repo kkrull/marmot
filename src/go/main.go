e74121373052ea84178c65a3225340d390be7633
Kyle Krull <kdkrull@gmail.com>
2024-07-18 14:16:55 -0500

    Read verison from a file

--- src/go/main.go
+++ src/go/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kkrull/marmot/cmd"
 )
 
 func main() {
-	rootCmd := cmd.RootCommand("0.0.1")
-	if executeErr := rootCmd.Execute(); executeErr != nil {
+	if version, versionErr := readVersion("version"); versionErr != nil {
+		fmt.Fprintln(os.Stderr, versionErr.Error())
 		os.Exit(1)
+	} else if mainErr := doMain(version); mainErr != nil {
+		fmt.Fprintln(os.Stderr, mainErr.Error())
+		os.Exit(1)
+	}
+}
+
+func doMain(version string) error {
+	rootCmd := cmd.RootCommand(version)
+	return rootCmd.Execute()
+}
+
+func readVersion(versionFilename string) (string, error) {
+	if versionBytes, readErr := os.ReadFile(versionFilename); readErr != nil {
+		return "", fmt.Errorf("failed to read version file %s; %w", versionFilename, readErr)
+	} else {
+		return string(versionBytes), nil
 	}
 }
