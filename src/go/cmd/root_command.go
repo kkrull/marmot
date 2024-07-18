5412b34cd93ec0161253033401d1a7d5cb6e4fcf
Kyle Krull <kdkrull@gmail.com>
2024-07-18 13:50:06 -0500

    Add --debug

--- src/go/cmd/root_command.go
+++ src/go/cmd/root_command.go
@@ -7,6 +7,7 @@ import (
 )
 
 var (
+	debugFlag   *bool
 	versionFlag *bool
 )
 
@@ -15,15 +16,31 @@ func RootCommand() *cobra.Command {
 	var rootCmd = &cobra.Command{
 		Long: `marmot manages a Meta Repository that organizes content in other (Git) repositories.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("--version: %v\n", *versionFlag)
+			if *debugFlag {
+				printDebug()
+			} else if *versionFlag {
+				printVersion()
+			}
 		},
 		Short: "Meta Repo Management Tool",
 		Use:   "marmot",
 	}
 
 	// Add flags (persistent and local)
-	versionFlag = rootCmd.LocalFlags().Bool("version", false, "print the marmot suite version")
+	debugFlag = rootCmd.PersistentFlags().Bool("debug", false, "print CLI debugging information")
+	versionFlag = rootCmd.PersistentFlags().Bool("version", false, "print the marmot suite version")
 
 	// Add child (sub-)commands
 	return rootCmd
+}
+
+/* Pseudo-commands */
+
+func printDebug() {
+	fmt.Printf("--debug: %v\n", *debugFlag)
+	fmt.Printf("--version: %v\n", *versionFlag)
+}
+
+func printVersion() {
+	fmt.Printf("marmot (go) 0.0.1")
 }
