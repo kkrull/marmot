0ed37be1a089f5c7a81b182a90f152efb9dd0efc
Kyle Krull <kdkrull@gmail.com>
2024-07-22 16:52:50 -0500

    Rename variables

--- src/go/cmd/init_command.go
+++ src/go/cmd/init_command.go
@@ -12,13 +12,13 @@ var initCmd = &cobra.Command{
 	GroupID: metaRepoGroup,
 	Long:    "Initialize a blank Meta Repo in the configured directory, if none is already present.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if factory, factoryErr := mainfactory.DefaultAppFactory(); factoryErr != nil {
-			return factoryErr
-		} else if initUseCmd, initErr := factory.InitCommand(); initErr != nil {
-			return fmt.Errorf("failed to create command; %w", initErr)
+		if appFactory, configErr := mainfactory.DefaultAppFactory(); configErr != nil {
+			return configErr
+		} else if initAppCmd, buildErr := appFactory.InitCommand(); buildErr != nil {
+			return fmt.Errorf("failed to create command; %w", buildErr)
 		} else if metaRepoHome, pathErr := mainfactory.DefaultMetaRepoPath(); pathErr != nil {
 			return pathErr
-		} else if runErr := initUseCmd.Run(metaRepoHome); runErr != nil {
+		} else if runErr := initAppCmd.Run(metaRepoHome); runErr != nil {
 			return fmt.Errorf("failed to initialize meta repo; %w", runErr)
 		} else {
 			fmt.Printf("Initialized meta repo at %s\n", metaRepoHome)
