5e42d65c1f4069e82d187f40b4e0f17973e0e72c
Kyle Krull <kdkrull@gmail.com>
2024-07-24 14:18:55 -0500

    Organize run

--- src/go/cmd/root_command.go
+++ src/go/cmd/root_command.go
@@ -13,19 +13,8 @@ var (
 // Configure the root command with the given I/O and version identifier, then return for use.
 func NewRootCommand(stdout io.Writer, stderr io.Writer, version string) (*cobra.Command, error) {
 	rootCmd = &cobra.Command{
-		Long: "marmot manages a Meta Repository that organizes content in other (Git) repositories.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if config, parseErr := ParseFlags(cmd); parseErr != nil {
-				return parseErr
-			} else if config.Debug {
-				config.PrintDebug(stdout)
-				return nil
-			} else if len(args) == 0 {
-				return cmd.Help()
-			} else {
-				return nil
-			}
-		},
+		Long:    "marmot manages a Meta Repository that organizes content in other (Git) repositories.",
+		RunE:    runRoot,
 		Short:   "Meta Repo Management Tool",
 		Use:     "marmot",
 		Version: version,
@@ -41,6 +30,19 @@ func NewRootCommand(stdout io.Writer, stderr io.Writer, version string) (*cobra.
 	rootCmd.SetOut(stdout)
 	rootCmd.SetErr(stderr)
 	return rootCmd, nil
+}
+
+func runRoot(cobraCmd *cobra.Command, args []string) error {
+	if config, parseErr := ParseFlags(cobraCmd); parseErr != nil {
+		return parseErr
+	} else if config.Debug {
+		config.PrintDebug(cobraCmd.OutOrStdout())
+		return nil
+	} else if len(args) == 0 {
+		return cobraCmd.Help()
+	} else {
+		return nil
+	}
 }
 
 /* Child commands */
--- src/go/cmdinit/init_command.go
+++ src/go/cmdinit/init_command.go
@@ -21,27 +21,31 @@ func (cliCmd *initCommand) RegisterWithCobra() {
 
 func (cliCmd *initCommand) toCobraCommand() *cobra.Command {
 	return &cobra.Command{
-		Args: cobra.NoArgs,
-		Long: "Initialize a new Meta Repo, if none is already present.",
-		RunE: func(cobraCmd *cobra.Command, _args []string) error {
-			if config, parseErr := cmd.ParseFlags(cobraCmd); parseErr != nil {
-				return parseErr
-			} else if config.Debug {
-				config.PrintDebug(cobraCmd.OutOrStdout())
-				return nil
-			} else if runErr := run(config); runErr != nil {
-				return runErr
-			} else {
-				fmt.Printf("Initialized meta repo at %s\n", config.MetaRepoPath)
-				return nil
-			}
-		},
+		Args:  cobra.NoArgs,
+		Long:  "Initialize a new Meta Repo, if none is already present.",
+		RunE:  runInit,
 		Short: "Initialize a meta repo",
 		Use:   "init",
 	}
 }
 
-func run(config *cmd.Config) error {
+func runInit(cobraCmd *cobra.Command, _args []string) error {
+	if config, parseErr := cmd.ParseFlags(cobraCmd); parseErr != nil {
+		return parseErr
+	} else if config.Debug {
+		config.PrintDebug(cobraCmd.OutOrStdout())
+		return nil
+	} else {
+		return runInitAppCmd(cobraCmd, config)
+	}
+}
+
+func runInitAppCmd(cobraCmd *cobra.Command, config *cmd.Config) error {
 	initAppCmd := config.AppFactory.InitCommand()
-	return initAppCmd.Run(config.MetaRepoPath)
+	if runErr := initAppCmd.Run(config.MetaRepoPath); runErr != nil {
+		return runErr
+	} else {
+		fmt.Fprintf(cobraCmd.OutOrStdout(), "Initialized meta repo at %s\n", config.MetaRepoPath)
+		return nil
+	}
 }
