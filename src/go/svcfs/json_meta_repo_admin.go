c8d6eb97423f45b2320b11d6163572e49f5a6d64
Kyle Krull <kdkrull@gmail.com>
2024-07-25 09:33:23 -0500

    Prepare for using a real version number

--- src/go/svcfs/json_meta_repo_admin.go
+++ src/go/svcfs/json_meta_repo_admin.go
@@ -8,18 +8,25 @@ import (
 )
 
 func NewJsonMetaRepoAdmin() *JsonMetaRepoAdmin {
+	// TODO KDK: Add version parameter
 	return &JsonMetaRepoAdmin{}
 }
 
 // Creates meta repos that store meta data in JSON files on the local file system.
-type JsonMetaRepoAdmin struct{}
+type JsonMetaRepoAdmin struct {
+	version string
+}
 
 /* MetaDataAdmin */
 
-func (*JsonMetaRepoAdmin) Create(repositoryDir string) error {
+func (admin *JsonMetaRepoAdmin) Create(repositoryDir string) error {
 	_, statErr := os.Stat(repositoryDir)
 	if errors.Is(statErr, fs.ErrNotExist) {
-		return initDirectory(metaDataDir(repositoryDir), metaDataFile(repositoryDir))
+		return initDirectory(
+			metaDataDir(repositoryDir),
+			metaDataFile(repositoryDir),
+			InitMetaRepoData(admin.version),
+		)
 	} else if statErr != nil {
 		return fmt.Errorf("failed to check for existing meta repo %s; %w", repositoryDir, statErr)
 	} else {
@@ -27,12 +34,11 @@ func (*JsonMetaRepoAdmin) Create(repositoryDir string) error {
 	}
 }
 
-func initDirectory(metaDataDirS string, metaDataFileS string) error {
-	emptyFile := EmptyMetaRepoFile("0.0.1") //TODO KDK: Get the real version number
-	if dirErr := os.MkdirAll(metaDataDirS, fs.ModePerm); dirErr != nil {
-		return fmt.Errorf("failed to make directory %s; %w", metaDataDirS, dirErr)
-	} else if writeErr := emptyFile.WriteTo(metaDataFileS); writeErr != nil {
-		return fmt.Errorf("failed to write file %s; %w", metaDataFileS, writeErr)
+func initDirectory(metaDataDir string, metaDataFile string, rootObject *rootObjectData) error {
+	if dirErr := os.MkdirAll(metaDataDir, fs.ModePerm); dirErr != nil {
+		return fmt.Errorf("failed to make directory %s; %w", metaDataDir, dirErr)
+	} else if writeErr := rootObject.WriteTo(metaDataFile); writeErr != nil {
+		return fmt.Errorf("failed to write file %s; %w", metaDataFile, writeErr)
 	} else {
 		return nil
 	}
