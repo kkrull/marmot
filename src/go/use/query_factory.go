42318fdcc484c354a8033b634df6e8d5120cab30
Kyle Krull <kdkrull@gmail.com>
2024-07-30 18:22:21 -0400

    Sprout QueryFactory

---
+++ src/go/use/query_factory.go
@@ -0,0 +1,51 @@
+package use
+
+import (
+	"errors"
+
+	"github.com/kkrull/marmot/coremetarepo"
+	"github.com/kkrull/marmot/corerepository"
+	"github.com/kkrull/marmot/userepository"
+)
+
+func NewQueryFactory() *queryFactory {
+	return &queryFactory{}
+}
+
+// Constructs application queries with configurable services.
+type QueryFactory interface {
+	ListRemoteRepositoriesQuery() (userepository.ListRemoteRepositoriesQuery, error) //TODO KDK: Remove "Query"
+}
+
+type queryFactory struct {
+	MetaDataAdmin    coremetarepo.MetaDataAdmin
+	RepositorySource corerepository.RepositorySource
+}
+
+func (factory *queryFactory) WithMetaDataAdmin(admin coremetarepo.MetaDataAdmin) *queryFactory {
+	factory.MetaDataAdmin = admin
+	return factory
+}
+
+func (factory *queryFactory) WithRepositorySource(source corerepository.RepositorySource) *queryFactory {
+	factory.RepositorySource = source
+	return factory
+}
+
+/* Repositories */
+
+func (factory *queryFactory) ListRemoteRepositoriesQuery() (userepository.ListRemoteRepositoriesQuery, error) {
+	if repositorySource, err := factory.repositorySource(); err != nil {
+		return nil, err
+	} else {
+		return repositorySource.ListRemote, nil
+	}
+}
+
+func (factory *queryFactory) repositorySource() (corerepository.RepositorySource, error) {
+	if factory.RepositorySource == nil {
+		return nil, errors.New("QueryFactory: missing RepositorySource")
+	} else {
+		return factory.RepositorySource, nil
+	}
+}
