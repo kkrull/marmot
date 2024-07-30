71bf0ab72865659334ca5a8fac5cbf22b272e1ae
Kyle Krull <kdkrull@gmail.com>
2024-07-30 18:39:19 -0400

    Group functions in AppConfig

--- src/go/cmd/root/app_config.go
+++ src/go/cmd/root/app_config.go
@@ -9,12 +9,37 @@ import (
 
 // Abstract factory that creates various application factories from a CLI configuration.
 type AppConfig interface {
+	/* Application */
+
+	//Constructs application commands
 	AppFactory() use.AppFactory
+
+	/* CLI arguments */
+
+	//Positional arguments that weren't parsed as flags, as raw text.
 	Args() []string
+
+	//Positional arguments parsed as URLs.
 	ArgsAsUrls() ([]*url.URL, error)
-	Debug() bool
-	InputLines() []string
-	InputLinesAsUrls() ([]*url.URL, error)
-	MetaRepoPath() string
+
+	/* CLI Debugging */
+
+	//Print information for debugging CLI parsing to the given writer.
 	PrintDebug(writer io.Writer)
+
+	/* CLI flags */
+
+	//Persistent flag to show debugging information instead of parsing and executing commands.
+	Debug() bool
+
+	//Persistent flag for the path to the meta repo to use in any parsed commands or queries.
+	MetaRepoPath() string
+
+	/* CLI input */
+
+	//Lines of text input from another process–e.g. through a pipe to standard input–as raw text.
+	InputLines() []string
+
+	//Lines of text input from another process–e.g. through a pipe to standard input–parsed as URLs.
+	InputLinesAsUrls() ([]*url.URL, error)
 }
