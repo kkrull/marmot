917f26b6b915b4ef2885a4aad9bcda57604dbb75
Kyle Krull <kdkrull@gmail.com>
2024-08-06 11:52:59 -0500

    Document

--- src/go/coremetarepo/meta_data_admin.go
+++ src/go/coremetarepo/meta_data_admin.go
@@ -1,6 +1,7 @@
 package coremetarepo
 
-// Maintenance of a meta repository.
+// Manages the life cycle a meta repository.
 type MetaDataAdmin interface {
+	// Create a meta repository at the specified path on the local filesystem.
 	Create(metaRepoPath string) error
 }
