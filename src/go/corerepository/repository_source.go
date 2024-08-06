5aeeb5112bf7de55edc2b25412365d1cf95153d7
Kyle Krull <kdkrull@gmail.com>
2024-08-06 11:41:23 -0500

    Add task

--- src/go/corerepository/repository_source.go
+++ src/go/corerepository/repository_source.go
@@ -4,6 +4,7 @@ import "net/url"
 
 // A source of Git repositories that a meta repo might care about.
 type RepositorySource interface {
+	// TODO KDK: Consider adding #Add(Repository)
 	AddLocal(localPath string) error
 	AddRemote(hostUrl *url.URL) error
 	ListLocal() (Repositories, error)
