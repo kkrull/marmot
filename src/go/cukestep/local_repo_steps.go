5a215716f17e32865eb0db8f04b30cab2b27f440
Kyle Krull <kdkrull@gmail.com>
2024-08-06 09:38:52 -0500

    Fix bug in path expectations

--- src/go/cukestep/local_repo_steps.go
+++ src/go/cukestep/local_repo_steps.go
@@ -1,8 +1,8 @@
 package cukestep
 
 import (
-	"context"
 	"fmt"
+	"path/filepath"
 
 	"github.com/cucumber/godog"
 	support "github.com/kkrull/marmot/cukesupport"
@@ -16,16 +16,12 @@ var thoseLocalRepositories *support.LocalRepositories = support.NoLocalRepositor
 
 // Add step definitions related to repositories on the local filesystem.
 func AddLocalRepositorySteps(ctx *godog.ScenarioContext) {
-	ctx.After(func(ctx context.Context, _ *godog.Scenario, err error) (context.Context, error) {
-		if err != nil {
-			return ctx, err
-		} else {
-			return ctx, thoseLocalRepositories.DeleteAll()
-		}
-	})
-
 	ctx.Given(`^Git repositories on the local filesystem$`, func() error {
-		return createLocalRepo("empty-dir")
+		if testDir, pathErr := support.TestDir(); pathErr != nil {
+			return pathErr
+		} else {
+			return createLocalRepo(filepath.Join(testDir, "empty-dir"))
+		}
 	})
 
 	ctx.Given(`^I have registered those local repositories with a meta repo$`, registerThoseLocals)
