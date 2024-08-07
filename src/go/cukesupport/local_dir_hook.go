00388f5b4a8f633d3c37e8a17e38cd73fc0f45fc
Kyle Krull <kdkrull@gmail.com>
2024-08-07 09:13:03 -0500

    Make room for more hooks

--- src/go/cukesupport/local_dir_hook.go
+++ src/go/cukesupport/local_dir_hook.go
@@ -9,11 +9,10 @@ import (
 	"path/filepath"
 
 	"github.com/cucumber/godog"
-	messages "github.com/cucumber/messages/go/v21"
 )
 
 // The name of the tag to identify in feature files
-const TagName = "@LocalDir"
+const LocalDirTag = "@LocalDir"
 
 /* State */
 
@@ -22,7 +21,7 @@ var _testDir string
 // A temporary directory for scenarios tagged with `TagName`, to automatically delete afterwards.
 func TestDir() (string, error) {
 	if _testDir == "" {
-		return "", fmt.Errorf("[%s] not initialized", TagName)
+		return "", fmt.Errorf("[%s] not initialized", LocalDirTag)
 	}
 
 	return _testDir, nil
@@ -31,7 +30,7 @@ func TestDir() (string, error) {
 // The named subdirectory inside this scenario's temporary directory (which is deleted afterwards).
 func TestSubDir(subdir string) (string, error) {
 	if _testDir == "" {
-		return "", fmt.Errorf("[%s] not initialized", TagName)
+		return "", fmt.Errorf("[%s] not initialized", LocalDirTag)
 	}
 
 	return filepath.Join(_testDir, subdir), nil
@@ -45,45 +44,33 @@ func setTestDir(path string) {
 
 // Make this hook available to a scenario, if it is tagged with its name.
 func addLocalDirHook(ctx *godog.ScenarioContext) {
-	ctx.After(afterHook)
-	ctx.Before(beforeHook)
+	ctx.After(afterLocalDir)
+	ctx.Before(beforeLocalDir)
 }
 
-func afterHook(ctx context.Context, sc *godog.Scenario, err error) (context.Context, error) {
-	matchingTag := findTag(TagName, sc.Tags)
-	if nothingToDo := _testDir == ""; nothingToDo {
-		return ctx, err
-	} else if notApplicable := matchingTag == nil; notApplicable {
+func afterLocalDir(ctx context.Context, sc *godog.Scenario, err error) (context.Context, error) {
+	if _testDir == "" || findTag(LocalDirTag, sc.Tags) == nil {
 		return ctx, err
 	} else if rmErr := os.RemoveAll(_testDir); rmErr != nil {
-		return ctx, errors.Join(err, fmt.Errorf("%s: failed to remove %s; %w", TagName, _testDir, rmErr))
+		hookErr := fmt.Errorf("%s: failed to remove %s; %w", LocalDirTag, _testDir, rmErr)
+		return ctx, errors.Join(err, hookErr)
 	} else {
-		log.Printf("[%s] Removed test directory: %s\n", TagName, _testDir)
+		log.Printf("[%s] Removed test directory: %s\n", LocalDirTag, _testDir)
 		setTestDir("")
 		return ctx, err
 	}
 }
 
-func beforeHook(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
-	if hookTag := findTag(TagName, sc.Tags); hookTag == nil {
+func beforeLocalDir(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
+	if hookTag := findTag(LocalDirTag, sc.Tags); hookTag == nil {
 		return ctx, nil
 	} else if localDir, pathErr := filepath.Abs(filepath.Join(".", "localDir")); pathErr != nil {
-		return ctx, fmt.Errorf("%s: failed to determine path; %w", TagName, pathErr)
+		return ctx, fmt.Errorf("%s: failed to determine path; %w", LocalDirTag, pathErr)
 	} else if mkErr := os.MkdirAll(localDir, 0o777); mkErr != nil {
-		return ctx, fmt.Errorf("%s: failed to create %s; %w", TagName, localDir, mkErr)
+		return ctx, fmt.Errorf("%s: failed to create %s; %w", LocalDirTag, localDir, mkErr)
 	} else {
-		log.Printf("[%s] Created test directory: %s\n", TagName, localDir)
+		log.Printf("[%s] Created test directory: %s\n", LocalDirTag, localDir)
 		setTestDir(localDir)
 		return ctx, nil
 	}
-}
-
-func findTag(name string, tags []*messages.PickleTag) *messages.PickleTag {
-	for _, tag := range tags {
-		if tag.Name == name {
-			return tag
-		}
-	}
-
-	return nil
 }
