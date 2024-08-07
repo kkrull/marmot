0ceb258806955cdefbc88346f5dd735ee291ba12
Kyle Krull <kdkrull@gmail.com>
2024-08-07 09:18:11 -0500

    Push initialization to helper

--- src/go/cukestep/meta_repo_steps.go
+++ src/go/cukestep/meta_repo_steps.go
@@ -1,28 +1,12 @@
 package cukestep
 
 import (
-	"fmt"
-
 	"github.com/cucumber/godog"
 	support "github.com/kkrull/marmot/cukesupport"
 )
 
 // Add step definitions to manage the life cycle of a meta repo.
 func AddMetaRepoSteps(ctx *godog.ScenarioContext) {
-	ctx.Given(`^I have initialized a new meta repo$`, initNewMetaRepo)
-}
-
-/* Steps */
-
-func initNewMetaRepo() error {
-	factory := support.ThatCommandFactoryS("42")
-	if initCmd, factoryErr := factory.NewInitMetaRepo(); factoryErr != nil {
-		return fmt.Errorf("meta_repo_steps: failed to initialize; %w", factoryErr)
-	} else if thatMetaRepo, initErr := support.InitThatMetaRepo(); initErr != nil {
-		return fmt.Errorf("meta_repo_steps: failed to initialize path to meta repo; %w", initErr)
-	} else if runErr := initCmd.Run(thatMetaRepo); runErr != nil {
-		return fmt.Errorf("meta_repo_steps: failed to initialize repository; %w", runErr)
-	} else {
-		return nil
-	}
+	initWithArbitraryVersion := func() error { return support.InitNewMetaRepoS("42") }
+	ctx.Given(`^I have initialized a new meta repo$`, initWithArbitraryVersion)
 }
--- src/go/cukesupport/meta_repo_helper.go
+++ src/go/cukesupport/meta_repo_helper.go
@@ -8,43 +8,62 @@ import (
 	"github.com/cucumber/godog"
 )
 
+// The name of the tag to identify in feature files
+const MetaRepoTag = "@MetaRepo"
+
 // State to clear between scenarios
 var _thatMetaRepo string
 
 // Add fixtures to reset state between scenarios.
 func addMetaRepoFixture(ctx *godog.ScenarioContext) {
-	ctx.After(func(ctx context.Context, _ *godog.Scenario, err error) (context.Context, error) {
-		clearThatMetaRepo()
-		return ctx, err
-	})
+	ctx.After(afterMetaRepo)
+	ctx.Before(beforeMetaRepo)
 }
 
-// Reset the meta repo so another scenario can make its own.
-func clearThatMetaRepo() {
+func afterMetaRepo(ctx context.Context, sc *godog.Scenario, err error) (context.Context, error) {
+	forgetThatMetaRepo()
+	return ctx, err
+}
+
+func beforeMetaRepo(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
+	return ctx, nil
+}
+
+func forgetThatMetaRepo() {
 	_thatMetaRepo = ""
+}
+
+// Convenience method initializing a meta repo with this version.
+func InitNewMetaRepoS(version string) error {
+	factory := ThatCommandFactoryS(version)
+	if initCmd, factoryErr := factory.NewInitMetaRepo(); factoryErr != nil {
+		return fmt.Errorf("meta_repo_steps: failed to initialize; %w", factoryErr)
+	} else if thatMetaRepo, initErr := InitThatMetaRepo(); initErr != nil {
+		return fmt.Errorf("meta_repo_steps: failed to initialize path to meta repo; %w", initErr)
+	} else if runErr := initCmd.Run(thatMetaRepo); runErr != nil {
+		return fmt.Errorf("meta_repo_steps: failed to initialize repository; %w", runErr)
+	} else {
+		return nil
+	}
 }
 
 // Set a standardized path to a meta repo in the local test directory, adding a hook to clear it.
 func InitThatMetaRepo() (string, error) {
-	if existing := peekThatMetaRepo(); existing != "" {
-		return "", fmt.Errorf("meta_repo_helper: meta repo path already configured: %s", existing)
+	if _thatMetaRepo != "" {
+		return "", fmt.Errorf("meta_repo_helper: meta repo path already configured: %s", _thatMetaRepo)
 	} else if testDir, testDirErr := TestDir(); testDirErr != nil {
 		return "", fmt.Errorf("meta_repo_helper: failed to access test directory; %w", testDirErr)
 	} else {
 		setThatMetaRepo(filepath.Join(testDir, "meta"))
-		return peekThatMetaRepo(), nil
+		return _thatMetaRepo, nil
 	}
-}
-
-func peekThatMetaRepo() string {
-	return _thatMetaRepo
 }
 
 func setThatMetaRepo(path string) {
 	_thatMetaRepo = path
 }
 
-// A path to a meta repo which has been set earlier in this scenario.
+// A path to a meta repo which has been initialized earlier in this scenario.
 func ThatMetaRepo() (string, error) {
 	if _thatMetaRepo == "" {
 		return "", fmt.Errorf("meta_repo_helper: no meta repo has been configured")
