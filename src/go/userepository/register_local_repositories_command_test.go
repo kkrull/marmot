cdb8e53c954e2688b0b5803ec1a440e8109678c9
Kyle Krull <kdkrull@gmail.com>
2024-08-20 15:31:30 -0500

    Move test for os.Abs

--- src/go/userepository/register_local_repositories_command_test.go
+++ src/go/userepository/register_local_repositories_command_test.go
@@ -41,16 +41,6 @@ var _ = Describe("RegisterLocalRepositoriesCommand", func() {
 			source.AddLocalsExpected("/path/to/a", "/path/to/b")
 		})
 
-		It("returns no error, upon success", func() {
-			Expect(subject.Run(validPaths())).To(Succeed())
-		})
-
-		It("returns an error, when adding repositories fails", func() {
-			source.AddLocalsFails(errors.New("bang!"))
-			Expect(subject.Run(validPaths())).To(
-				MatchError(ContainSubstring("failed to add local repositories; bang!")))
-		})
-
 		It("normalizes paths by replacing redundant/repeated parts with shorter equivalents", func() {
 			subject.Run([]string{"/path/to/a/../b"})
 			source.AddLocalsExpected("/path/to/b")
@@ -60,6 +50,20 @@ var _ = Describe("RegisterLocalRepositoriesCommand", func() {
 			subject.Run([]string{"some-name-in-cwd"})
 			Expect(source.AddLocalsReceived()[0]).To(
 				HaveSuffix(string(os.PathSeparator) + "some-name-in-cwd"))
+		})
+
+		It("returns no error, upon success", func() {
+			Expect(subject.Run(validPaths())).To(Succeed())
+		})
+
+		//The cwd could disappear since this is a CLI program, which could cause os.Abs to fail.
+		//https://stackoverflow.com/a/75753434/112682
+		It("returns an error when the current working directory can not be obtained", Pending)
+
+		It("returns an error, when adding repositories fails", func() {
+			source.AddLocalsFails(errors.New("bang!"))
+			Expect(subject.Run(validPaths())).To(
+				MatchError(ContainSubstring("failed to add local repositories; bang!")))
 		})
 	})
 })
