2cac6c796d00e235ae4397fbd9a455dd9527cc79
Kyle Krull <kdkrull@gmail.com>
2025-01-24 00:47:15 -0600

    Add next step

--- src/go/cmd/local/local.go
+++ src/go/cmd/local/local.go
@@ -10,11 +10,11 @@ func NewLocalCmd() *cobra.Command {
 		Args:    cobra.NoArgs,
 		GroupID: cmdshared.RepositoryGroup.Id(),
 		Long:    "Deal with repositories on the local filesystem.",
-		Short: "Deal with local repositories",
-		Use:   "local",
+		Short:   "Deal with local repositories",
+		Use:     "local",
 	}
 
-	localCmd.AddCommand(NewListLocalCmd())
+	localCmd.AddCommand(NewListLocalCmd()) // TODO KDK: Wire up and run this command
 	localCmd.AddCommand(NewRegisterLocalCmd())
 	return localCmd
 }
