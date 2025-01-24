5f067ddb9c1f957be9e4a5587c13595fe786d7c6
Kyle Krull <kdkrull@gmail.com>
2025-01-24 15:24:33 -0600

    Make more reusable

--- src/go/cmd/init/init.go
+++ src/go/cmd/init/init.go
@@ -2,40 +2,66 @@ package cmdinit
 
 import (
 	"fmt"
+	"io"
 
 	cmdshared "github.com/kkrull/marmot/cmd/shared"
 	"github.com/spf13/cobra"
 )
 
-func NewInitCmd(group cmdshared.CommandGroup, parser cmdshared.CliConfigParser) *cobra.Command {
+func NewInitCmd(
+	group cmdshared.CommandGroup,
+	parser cmdshared.CliConfigParser,
+) *cobra.Command {
 	initCmd := &cobra.Command{
 		Args:    cobra.NoArgs,
 		GroupID: group.Id,
 		Long:    "Initialize a new Meta Repo, if none is already present.",
-		RunE:    newCobraCommandRunE(parser),
-		Short:   "Initialize a meta repo",
-		Use:     "init",
+		RunE: parseThen(
+			func(cmd *cobra.Command, args []string) (cmdshared.CliConfig, error) {
+				return parser.Parse(cmd.Flags(), args)
+			},
+			runInitAction(),
+		),
+		Short: "Initialize a meta repo",
+		Use:   "init",
 	}
 
 	return initCmd
 }
 
-type cobraRunner = func(cmd *cobra.Command, args []string) error
-
-func newCobraCommandRunE(parser cmdshared.CliConfigParser) cobraRunner {
-	return func(cli *cobra.Command, args []string) error {
-		if config, parseErr := parser.Parse(cli.Flags(), args); parseErr != nil {
-			return parseErr
-		} else if config.Debug() {
-			config.PrintDebug(cli.OutOrStdout())
+func runInitAction() useFn {
+	return func(config cmdshared.CliConfig, stdout io.Writer) error {
+		if config.Debug() {
+			config.PrintDebug(stdout)
 			return nil
 		} else if action, actErr := config.ActionFactory().NewInitMetaRepo(); actErr != nil {
 			return actErr
 		} else if runErr := action.Run(config.MetaRepoPath()); runErr != nil {
 			return runErr
 		} else {
-			fmt.Fprintf(cli.OutOrStdout(), "Initialized meta repo at %s\n", config.MetaRepoPath())
+			fmt.Fprintf(stdout, "Initialized meta repo at %s\n", config.MetaRepoPath())
 			return nil
 		}
 	}
 }
+
+/* General purpose */
+
+type cobraRunEFn = func(cli *cobra.Command, args []string) error
+
+func parseThen[TConfig any](
+	parseConfig func(*cobra.Command, []string) (TConfig, error),
+	useConfig func(TConfig, io.Writer) error,
+) cobraRunEFn {
+	return func(cli *cobra.Command, args []string) error {
+		if config, parseErr := parseConfig(cli, args); parseErr != nil {
+			return parseErr
+		} else if useErr := useConfig(config, cli.OutOrStdout()); useErr != nil {
+			return useErr
+		} else {
+			return nil
+		}
+	}
+}
+
+type useFn = func(cmdshared.CliConfig, io.Writer) error
